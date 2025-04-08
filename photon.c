0aa9d8fac444d4e0c0f50bc966900c2d3a06d54c
LautaroCerruti <lautarocerruti7@gmail.com>
2025-04-08 20:44:40 -0300

    rollback struct, added likeliness

--- photon.c
+++ photon.c
@@ -4,15 +4,8 @@
 #include "params.h"
 #include "xoshiro.h"
 
-typedef struct {
-    float x;
-    float y;
-    float z;
-    float u;
-    float v;
-    float w;
-    float weight; 
-} Photon;
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
 
 void photon(float* heats, float* heats_squared)
 {
@@ -20,21 +13,27 @@ void photon(float* heats, float* heats_squared)
     const float shells_per_mfp = 1e4 / MICRONS_PER_SHELL / (MU_A + MU_S);
 
     /* launch */
-    Photon p = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f};
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+    float u = 0.0f;
+    float v = 0.0f;
+    float w = 1.0f;
+    float weight = 1.0f;
 
     for (;;) {
         float t = -logf(next_float()); /* move */
-        p.x += t * p.u;
-        p.y += t * p.v;
-        p.z += t * p.w;
+        x += t * u;
+        y += t * v;
+        z += t * w;
 
-        unsigned int shell = sqrtf(p.x * p.x + p.y * p.y + p.z * p.z) * shells_per_mfp; /* absorb */
+        unsigned int shell = sqrtf(x * x + y * y + z * z) * shells_per_mfp; /* absorb */
         if (shell > SHELLS - 1) {
             shell = SHELLS - 1;
         }
-        heats[shell] += (1.0f - albedo) * p.weight;
-        heats_squared[shell] += (1.0f - albedo) * (1.0f - albedo) * p.weight * p.weight; /* add up squares */
-        p.weight *= albedo;
+        heats[shell] += (1.0f - albedo) * weight;
+        heats_squared[shell] += (1.0f - albedo) * (1.0f - albedo) * weight * weight; /* add up squares */
+        weight *= albedo;
 
         /* New direction, rejection method */
         float xi1, xi2;
@@ -43,14 +42,14 @@ void photon(float* heats, float* heats_squared)
             xi2 = 2.0f * next_float() - 1.0f;
             t = xi1 * xi1 + xi2 * xi2;
         } while (1.0f < t);
-        p.u = 2.0f * t - 1.0f;
-        p.v = xi1 * sqrtf((1.0f - p.u * p.u) / t);
-        p.w = xi2 * sqrtf((1.0f - p.u * p.u) / t);
+        u = 2.0f * t - 1.0f;
+        v = xi1 * sqrtf((1.0f - u * u) / t);
+        w = xi2 * sqrtf((1.0f - u * u) / t);
 
-        if (p.weight < 0.001f) { /* roulette */
-            if (next_float() > 0.1f)
+        if (unlikely(weight < 0.001f)) { /* roulette */
+            if (likely(next_float() > 0.1f))
                 break;
-            p.weight *= 10.0f;
+            weight /= 0.1f;
         }
     }
 }
